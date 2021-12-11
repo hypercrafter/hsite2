15ca8f8e85432ec43a47b5d5dd9334c7d7840694
hypercrafter <90453578+hypercrafter@users.noreply.github.com>
2021-12-11 13:21:14 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to my coding website!." />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          I made text coding projects</code>
         </p>
       </main>
 
